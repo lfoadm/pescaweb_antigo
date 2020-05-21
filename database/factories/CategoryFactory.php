6a24249c6498bfb1bb335c30741bacee8bff59f1
Leandro Oliveira <lfoadm@icloud.com>
2020-05-21 14:21:43 -0300

    Factory and Seed

--- database/factories/CategoryFactory.php
+++ database/factories/CategoryFactory.php
@@ -4,9 +4,13 @@
 
 use App\Model\Category;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Category::class, function (Faker $faker) {
+    $word = $faker->word;
+    $slug = Str::slug($word);
     return [
-        //
+        'name' => $word,
+        'slug' => $slug,
     ];
 });
--- database/factories/QuestionFactory.php
+++ database/factories/QuestionFactory.php
@@ -2,11 +2,25 @@
 
 /** @var \Illuminate\Database\Eloquent\Factory $factory */
 
+use App\Model\Category;
 use App\Model\Question;
+use App\User;
 use Faker\Generator as Faker;
+use Illuminate\Support\Str;
 
 $factory->define(Question::class, function (Faker $faker) {
+    $title = $faker->sentence;
+    $slug = Str::slug($title);
     return [
-        //
+        'title' => $title,
+        'slug'  => $title,
+        'body'  => $faker->text,
+        'category_id' => function () {
+            return Category::all()->random();
+        },
+        'user_id' => function () {
+            return User::all()->random();
+        }
+
     ];
 });
