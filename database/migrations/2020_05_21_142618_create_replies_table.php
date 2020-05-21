67df5e3f0726589f323d784a5988b83ecbc46b5d
Leandro Oliveira <lfoadm@icloud.com>
2020-05-21 13:29:14 -0300

    Relationships

--- database/migrations/2020_05_21_142618_create_replies_table.php
+++ database/migrations/2020_05_21_142618_create_replies_table.php
@@ -21,7 +21,7 @@ class CreateRepliesTable extends Migration
 
             $table->bigInteger('user_id');
 
-            $table->foreign('question_id')->references('id')->on('question')->onDelete('cascade');
+            $table->foreign('question_id')->references('id')->on('questions')->onDelete('cascade');
             $table->timestamps();
         });
     }
