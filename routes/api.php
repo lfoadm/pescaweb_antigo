0fe86df520d0dacce7e2720918adcbad34917f1f
Leandro Oliveira <lfoadm@icloud.com>
2020-05-21 18:36:35 -0300

    CRUD for Reply

--- routes/api.php
+++ routes/api.php
@@ -6,3 +6,5 @@ use Illuminate\Support\Facades\Route;
 Route::apiResource('/question', 'QuestionController');
 
 Route::apiResource('/category', 'CategoryController');
+
+Route::apiResource('/question/{question}/reply', 'ReplyController');
