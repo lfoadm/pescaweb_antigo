46da2acea11b1bd4ceec29b0fc787f2db0b08801
Leandro Oliveira <lfoadm@icloud.com>
2020-05-22 14:30:35 -0300

    JWT Package

---
+++ database/seeds/UsersTableSeeder.php
@@ -0,0 +1,24 @@
+<?php
+
+use Illuminate\Database\Seeder;
+use App\User;
+use Illuminate\Support\Str;
+
+class UsersTableSeeder extends Seeder
+{
+    /**
+     * Run the database seeds.
+     *
+     * @return void
+     */
+    public function run()
+    {
+        User::create([
+            'name'              => 'Leandro Oliveira',
+            'email'             => 'lfoadm@icloud.com',
+            'email_verified_at' => now(),
+            'password'          => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
+            'remember_token'    => Str::random(10),
+            ]);
+    }
+}
